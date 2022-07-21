08d3285fac7a4e9dae992aad5787182ba51c3db1
ALLADINDELAPADEV <104471458+ALLADINDELAPADEV@users.noreply.github.com>
2022-07-21 11:11:38 -0700

    for my review

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+// 3 variables 
+
+console.log(weeklyExpenseQuestions)
+
+ for(let i = 0; i < weeklyExpenseQuestions.length; i++)
+ 
+ {
+
+    let response = parseInt(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + response
+    
+    console.log(weeklyExpenses + response)
+    console.log(weeklyExpenses)
+
+ }
